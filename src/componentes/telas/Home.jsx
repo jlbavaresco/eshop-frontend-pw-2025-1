06405d248803224ef15bc53d8e4c0dbaf307035d
Jorge Luis Boeira Bavaresco <jorge@JorgeG15debian.IFSUL>
2025-04-04 11:07:14 -0300

    commit inicial do projeto

---
+++ src/componentes/telas/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <>
+            <h1>Seja bem vindo ao sistema eShop - PW</h1>
+        </>
+    )
+}
+
+export default Home;
---
+++ src/componentes/telas/Sobre.jsx
@@ -0,0 +1,10 @@
+const Sobre = () => {
+    return (
+        <>
+            <h1>Sobre o sistema....</h1>
+            <h2>Sistema desenvolvido na disciplina de PW - 2025-1</h2>
+        </>
+    )
+}
+
+export default Sobre;
